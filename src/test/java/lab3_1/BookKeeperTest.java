91e5eecd797f6260cf4178c508fbc47c29e8c2ef
Pelka <wojciech.pelka@accenture.com>
2016-09-11 19:52:44 +0200

    Dodanie pierwszego przypadku testowego dla BookKeeper

---
+++ src/test/java/lab3_1/BookKeeperTest.java
@@ -0,0 +1,56 @@
+package lab3_1;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Date;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.BookKeeper;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceFactory;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.InvoiceRequest;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.Tax;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.TaxPolicy;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+public class BookKeeperTest {
+
+	private BookKeeper bookKeeper;
+	private InvoiceFactory invoiceFactory;
+	private TaxPolicy taxPolicy;
+	private InvoiceRequest invoiceRequest;
+
+	@Before
+	public void start() {
+		taxPolicy = Mockito.mock(TaxPolicy.class);
+		invoiceFactory = new InvoiceFactory();
+		bookKeeper = new BookKeeper(invoiceFactory);
+		invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(), "Client"));
+	}
+
+	@Test
+	public void firstTestCase() {
+		Mockito.when(taxPolicy.calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class)))
+			   .thenReturn(new Tax(new Money(new BigDecimal(1000), Currency.getInstance("EUR")), "Podatek za cos tam."));
+
+		ProductData productData = new ProductData(Id.generate(),new Money(new BigDecimal(1000), Currency.getInstance("EUR")), "Standard", ProductType.STANDARD,new Date());
+		Money totalCost = new Money(new BigDecimal(10000), Currency.getInstance("EUR"));
+		RequestItem item = new RequestItem(productData, 10, totalCost);
+		invoiceRequest.add(item);
+
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		assertThat(invoice.getItems().size(), Matchers.is(1));
+		assertThat(invoice.getItems().get(0).getProduct(), Matchers.is(productData));
+	}
+
+}
